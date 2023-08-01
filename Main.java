d865e4f02b63af7733e6a64ffc05f2195b7c0f19
Tatiana25 <Tanya375293763203@outlook.com>
2023-08-01 21:35:02 +0300

    DZ_5_JAVA

--- Main.java
+++ Main.java
@@ -3,43 +3,92 @@ package org.homework5;
 import java.util.*;
 
 public class Main {
+    static class Notebook {
+        private String model;
+        private int RAM;
+        private int HDDSize;
+        private String operatingSystem;
+        private String color;
+
+        public Notebook(String model, int RAM, int HDDSize, String operatingSystem, String color) {
+            this.model = model;
+            this.RAM = RAM;
+            this.HDDSize = HDDSize;
+            this.operatingSystem = operatingSystem;
+            this.color = color;
+        }
+
+        public String toString() {
+            return "Модель: " + model + ", ОЗУ: " + RAM + " GB, Размер жесткого диска: " + HDDSize + " GB, ОС: " + operatingSystem + ", Цвет: " + color;
+        }
+    }
+
     public static void main(String[] args) {
-        HashMap<String, List<String>> phoneBook = new HashMap<>();
-        addContact(phoneBook, "Иванов", "123456789");
-        addContact(phoneBook, "Петров", "554433221");
-        addContact(phoneBook, "Сидоров", "111111111");
-        addContact(phoneBook, "Сидоров", "333333333");
-        addContact(phoneBook, "Васильев", "777777777");
-        addContact(phoneBook, "Сидоров", "222222222");
-        addContact(phoneBook, "Иванов", "987654321");
-       
-        printPhoneBook(phoneBook);
+
+        Set<Notebook> notebooks = new HashSet<>();
+
+        // Fill the set with notebooks
+        notebooks.add(new Notebook("Acer", 8, 512, "Windows 10", "Gray"));
+        notebooks.add(new Notebook("Asus", 16, 1000, "Windows 11", "Gray"));
+        notebooks.add(new Notebook("Lenovo", 16, 256, "Windows 11", "Black"));
+        notebooks.add(new Notebook("HP", 4, 500, "Windows 10", "Gray"));
+        notebooks.add(new Notebook("Dell", 8, 1000, "Windows 10", "Black"));
+
+        filterNotebooks(notebooks);
     }
 
-    public static void addContact(HashMap<String, List<String>> phoneBook, String name, String phoneNumber) {
-        if (phoneBook.containsKey(name)) {
-            List<String> numbers = phoneBook.get(name);
-            numbers.add(phoneNumber);
-        } else {
-            List<String> numbers = new ArrayList<>();
-            numbers.add(phoneNumber);
-            phoneBook.put(name, numbers);
+    public static void filterNotebooks(Set<Notebook> notebooks) {
+
+        Map<Integer, String> criteria = new HashMap<>();
+        criteria.put(1, "ОЗУ");
+        criteria.put(2, "Размер жесткого диска");
+        criteria.put(3, "Операционная система");
+        criteria.put(4, "Цвет");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите критерии фильтрации:");
+        for (Map.Entry<Integer, String> entry : criteria.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
         }
-    }
-    public static void printPhoneBook(HashMap<String, List<String>> phoneBook) {
-        List<Map.Entry<String, List<String>>> entries = new ArrayList<>(phoneBook.entrySet());
 
-        Collections.sort(entries, new Comparator<Map.Entry<String, List<String>>>() {
-            @Override
-            public int compare(Map.Entry<String, List<String>> o1, Map.Entry<String, List<String>> o2) {
-                return Integer.compare(o2.getValue().size(), o1.getValue().size());
+        int selectedCriteria = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Введите значение выбранных критериев:");
+        String value = scanner.nextLine();
+
+        Set<Notebook> filteredNotebooks = new HashSet<>();
+        for (Notebook notebook : notebooks) {
+            switch (selectedCriteria) {
+                case 1:
+                    if (String.valueOf(notebook.RAM).equals(value)) {
+                        filteredNotebooks.add(notebook);
+                    }
+                    break;
+                case 2:
+                    if (String.valueOf(notebook.HDDSize).equals(value)) {
+                        filteredNotebooks.add(notebook);
+                    }
+                    break;
+                case 3:
+                    if (notebook.operatingSystem.equalsIgnoreCase(value)) {
+                        filteredNotebooks.add(notebook);
+                    }
+                    break;
+                case 4:
+                    if (notebook.color.equalsIgnoreCase(value)) {
+                        filteredNotebooks.add(notebook);
+                    }
+                    break;
+                default:
+                    System.out.println("Недопустимые критерии фильтрации.");
+                    return;
             }
-        });
-        for (Map.Entry<String, List<String>> entry : entries) {
-            String name = entry.getKey();
-            List<String> numbers = entry.getValue();
-            System.out.println(name + ": " + numbers);
+        }
+
+        System.out.println("Отфильтрованные данные:");
+        for (Notebook notebook : filteredNotebooks) {
+            System.out.println(notebook);
         }
     }
-
 }
